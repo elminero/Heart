197e6231dabcb85a83a5d27a6129fc2a1b97966d
Robert Farber <elminero@cox.net>
2019-08-20 06:14:35 +0000

    Change to Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Heart
+                    Heart Git
                 </div>
 
                 <div class="links">
